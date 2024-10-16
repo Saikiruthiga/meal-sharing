0e31c6d54392a157a302479a788da7294cbcb178
Saikiruthiga <saikiruthiga06@gmail.com>
2024-10-16 09:30:38 +0200

    some configuration changed

--- api/eslint.config.js
+++ api/eslint.config.js
@@ -1,7 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import prettier from "eslint-config-prettier"; // Import Prettier config
+import prettierPlugin from "eslint-plugin-prettier"; // Prettier as a plugin
 
 export default [
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
+  prettier, // Disables conflicting ESLint rules
+  prettierPlugin.configs.recommended, // Use Prettier as an ESLint rule
 ];
